2ebf448438e966112a76a9f258f5e43c8bcc4c7d
shitalsa307-cpu <shitalsa307@gmail.com>
2026-05-30 22:25:41 +0530

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,8 +1,9 @@
 export default function handler(req, res) {
+  console.log("deploy fix");
+
   res.status(200).json({
     message: "OAuth Success",
     code: req.query.code,
-    state: req.query.state,
-    updated: true
+    state: req.query.state
   });
 }
